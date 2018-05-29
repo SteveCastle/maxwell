33c7f04df4d5aa49c11bc318bcf6cd960a62f70f
Steve Castle <steve@tigerhaunt.com>
2018-05-29 13:07:46 -0700

    configure s3 integration and flags on test cmd

--- cmd/lambda/main.go
+++ cmd/lambda/main.go
@@ -1,19 +1,19 @@
 package main
 
 import (
+	"context"
+	"fmt"
+
+	"github.com/aws/aws-lambda-go/events"
 	"github.com/aws/aws-lambda-go/lambda"
 )
 
-type Response struct {
-	Message string `json:"message"`
+func handler(ctx context.Context, s3Event events.S3Event) {
+	for _, record := range s3Event.Records {
+		s3 := record.S3
+		fmt.Printf("[%s - %s] Bucket = %s, Key = %s \n", record.EventSource, record.EventTime, s3.Bucket.Name, s3.Object.Key)
+	}
 }
-
-func Handler() (Response, error) {
-	return Response{
-		Message: "Go Serverless v1.0! Your function executed successfully!",
-	}, nil
-}
-
 func main() {
-	lambda.Start(Handler)
+	lambda.Start(handler)
 }
--- cmd/local/main.go
+++ cmd/local/main.go
@@ -1,39 +1,50 @@
 package main
 
 import (
+	"flag"
 	"fmt"
+	"os"
 
 	"github.com/aws/aws-sdk-go/aws"
-	"github.com/aws/aws-sdk-go/aws/awserr"
 	"github.com/aws/aws-sdk-go/aws/session"
-	"github.com/aws/aws-sdk-go/service/s3"
+	"github.com/aws/aws-sdk-go/service/s3/s3manager"
 )
 
+func exitErrorf(msg string, args ...interface{}) {
+	fmt.Fprintf(os.Stderr, msg+"\n", args...)
+	os.Exit(1)
+}
+
 func main() {
-	fmt.Println("vim-go")
+	// Get bucket and filename from command line
+	bucket := flag.String("bucket", "maxwell-go", "S3 bucket to perform operations on")
+	filename := flag.String("filename", "/Users/tracer/Downloads/wbvxqpwfunz01.jpg", "File to upload")
+	key := flag.String("key", "/images/target.jpg", "key for upload target on s3")
+	mimeType := flag.String("mimeType", "images/jpeg", "mimeType to use for s3 upload")
+	flag.Parse()
+	//Open file to be uploaded
+	file, err := os.Open(*filename)
+	if err != nil {
+		exitErrorf("Unable to open file %q, %v", err)
+	}
+
+	defer file.Close()
 	sess, err := session.NewSession(&aws.Config{
 		Region: aws.String("us-east-1")})
 	if err != nil {
 		fmt.Println(err)
 	}
-
-	svc := s3.New(sess)
-	// Example sending a request using the ListBucketsRequest method.
-	input := &s3.ListBucketsInput{}
-	result, err := svc.ListBuckets(input)
+	uploader := s3manager.NewUploader(sess)
+	_, err = uploader.Upload(&s3manager.UploadInput{
+		Bucket:      aws.String(*bucket),
+		Key:         aws.String(*key),
+		Body:        file,
+		ContentType: aws.String(*mimeType),
+	})
 	if err != nil {
-		if aerr, ok := err.(awserr.Error); ok {
-			switch aerr.Code() {
-			default:
-				fmt.Println(aerr.Error())
-			}
-		} else {
-			// Print the error, cast err to awserr.Error to get the Code and
-			// Message from an error.
-			fmt.Println(err.Error())
-		}
-		return
+		// Print the error and exit.
+		exitErrorf("Unable to upload %q to %q, %v", *filename, *bucket, err)
 	}
 
-	fmt.Println(result)
+	fmt.Printf("Successfully uploaded %q to %q\n", *filename, *bucket)
 }
