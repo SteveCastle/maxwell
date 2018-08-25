554f1e3867f60df88a9e6cf7f27a500884caeb3d
Steve Castle <steve@tigerhaunt.com>
2018-08-25 16:51:47 -0700

    make concurrent

--- cmd/refresh/main.go
+++ cmd/refresh/main.go
@@ -7,6 +7,7 @@ import (
 	"io/ioutil"
 	"os"
 	"strings"
+	"sync"
 
 	"github.com/SteveCastle/maxwell"
 	"github.com/aws/aws-sdk-go/aws"
@@ -26,12 +27,11 @@ type OutputConfig struct {
 }
 
 func main() {
-
 	//settings
 	bucket := flag.String("bucket", "maxwell-go", "Bucket to connect to.")
 	region := flag.String("region", "us-east-1", "Region of the bucket to connect to.")
 	sourcePath := flag.String("source", "images/", "Prefix to get input objects.")
-	cachePath := flag.String("cache", "maxwell-cache", "Prefix to write output objects.")
+	cachePath := flag.String("cache", "maxwell-cache/", "Prefix to write output objects.")
 	config := flag.String("config", "config.json", "Config file for output")
 
 	// Open the config file and parse it to an array of OutputConfig structs.
@@ -71,54 +71,62 @@ func main() {
 	// Iterate over objects in bucket with prefix.
 	fmt.Printf("Number of objects in set: %d\n", len(resp.Contents))
 	fmt.Println(resp)
+	var wg sync.WaitGroup
 	for _, record := range resp.Contents {
-		fmt.Printf("Working on: %s\n", *record.Key)
-
-		//Create a file to hold original image data.
-		file, err := os.Create("/tmp/file.jpg")
-		if err != nil {
-			exitErrorf("Unable to open file %q, %v", err)
-		}
-		defer file.Close()
-
-		// Download file and write to file.
-		numBytes, err := downloader.Download(file,
-			&s3.GetObjectInput{
-				Bucket: aws.String(*bucket),
-				Key:    aws.String(*record.Key),
-			})
-		if err != nil {
-			exitErrorf("Unable to download item %q, %v", *record.Key, err)
-		}
-
-		fmt.Println("Downloaded", numBytes, "bytes")
-
-		// process and upload defined file sizes.
-		// TODO: seperate processing and uploading and make concurrent.
-		for _, o := range outputs {
-			if o.Type == "square" {
-				maxwell.UploadToS3(maxwell.SquareResize(file, o.Size),
-					*bucket, fmt.Sprintf("%s/%s_%dw.jpg", *cachePath, maxwell.Basename(*record.Key), o.Size), uploader)
-			}
-			if o.Type == "svg" {
-				// Create a blurred svg.
-				svg := maxwell.ConvertToSVG("/tmp/file.jpg")
-
-				_, err = uploader.Upload(&s3manager.UploadInput{
-					Bucket: aws.String(*bucket),
-					Key: aws.String((fmt.Sprintf("%s/%s.svg",
-						*cachePath,
-						maxwell.Basename(*record.Key)))),
-					Body:        strings.NewReader(svg),
-					ContentType: aws.String("image/svg+xml"),
-				})
-				if err != nil {
-					// Print the error and exit.
-					exitErrorf("Unable to upload %q to %q, %v", *record.Key, *bucket, err)
-				}
-			}
-		}
-
-		fmt.Printf("Successfully uploaded %q to %q\n", *record.Key, *bucket)
+		wg.Add(1)
+		go processFile(&wg, record, bucket, cachePath, outputs, downloader, uploader)
 	}
+	wg.Wait()
+}
+func processFile(wg *sync.WaitGroup, record *s3.Object, bucket *string, cachePath *string, outputs []OutputConfig, downloader *s3manager.Downloader, uploader *s3manager.Uploader) {
+	defer wg.Done()
+	fmt.Printf("Working on: %s\n", *record.Key)
+
+	//Create a file to hold original image data.
+	fName := fmt.Sprintf("/tmp/%s", maxwell.Basename(*record.Key))
+	file, err := os.Create(fName)
+	if err != nil {
+		exitErrorf("Unable to open file %q, %v", err)
+	}
+	defer file.Close()
+
+	// Download file and write to file.
+	numBytes, err := downloader.Download(file,
+		&s3.GetObjectInput{
+			Bucket: aws.String(*bucket),
+			Key:    aws.String(*record.Key),
+		})
+	if err != nil {
+		exitErrorf("Unable to download item %q, %v", *record.Key, err)
+	}
+
+	fmt.Println("Downloaded", numBytes, "bytes")
+
+	// process and upload defined file sizes.
+	// TODO: seperate processing and uploading and make concurrent.
+	for _, o := range outputs {
+		if o.Type == "square" {
+			maxwell.UploadToS3(maxwell.SquareResize(file, o.Size),
+				*bucket, fmt.Sprintf("%s/%s_%dw.jpg", *cachePath, maxwell.Basename(*record.Key), o.Size), uploader)
+		}
+		if o.Type == "svg" {
+			// Create a blurred svg.
+			svg := maxwell.ConvertToSVG(fName)
+
+			_, err = uploader.Upload(&s3manager.UploadInput{
+				Bucket: aws.String(*bucket),
+				Key: aws.String((fmt.Sprintf("%s/%s.svg",
+					*cachePath,
+					maxwell.Basename(*record.Key)))),
+				Body:        strings.NewReader(svg),
+				ContentType: aws.String("image/svg+xml"),
+			})
+			if err != nil {
+				// Print the error and exit.
+				exitErrorf("Unable to upload %q to %q, %v", *record.Key, *bucket, err)
+			}
+		}
+	}
+
+	fmt.Printf("Successfully uploaded %q to %q\n", *record.Key, *bucket)
 }
