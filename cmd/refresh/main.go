5979657f2f21063ef93d63aea9ec2feec243c3df
Steve Castle <steve@tigerhaunt.com>
2018-08-25 11:32:34 -0700

    convert to go modules for 1.11 and refactoring continues

--- cmd/refresh/main.go
+++ cmd/refresh/main.go
@@ -1,8 +1,10 @@
 package main
 
 import (
+	"encoding/json"
 	"flag"
 	"fmt"
+	"io/ioutil"
 	"os"
 	"strings"
 
@@ -18,16 +20,38 @@ func exitErrorf(msg string, args ...interface{}) {
 	os.Exit(1)
 }
 
+type OutputConfig struct {
+	Type string
+	Size int
+}
+
 func main() {
 
 	//settings
-	bucket := flag.String("bucket", "notable-vegas", "Bucket to connect to")
-	sourcePath := flag.String("source", "public/media", "Prefix to get input objects.")
-	cachePath := flag.String("cache", "public/maxwell-cache", "Prefix to write output objects.")
+	bucket := flag.String("bucket", "maxwell-go", "Bucket to connect to.")
+	region := flag.String("region", "us-east-1", "Region of the bucket to connect to.")
+	sourcePath := flag.String("source", "images/", "Prefix to get input objects.")
+	cachePath := flag.String("cache", "maxwell-cache", "Prefix to write output objects.")
+	config := flag.String("config", "config.json", "Config file for output")
 
+	// Open the config file and parse it to an array of OutputConfig structs.
+	// We will iterate over this to create the final output files in the cache.
+
+	file, err := ioutil.ReadFile(*config)
+	if err != nil {
+		fmt.Println("Error reading config file.")
+		return
+	}
+	var outputs []OutputConfig
+
+	err = json.Unmarshal(file, &outputs)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
 	// Initialize session, s3, uploader, and downloader.
 	sess, err := session.NewSession(&aws.Config{
-		Region: aws.String("us-west-2")})
+		Region: aws.String(*region)})
 	if err != nil {
 		fmt.Println(err)
 	}
@@ -40,17 +64,20 @@ func main() {
 		Bucket: aws.String(*bucket),
 		Prefix: aws.String(*sourcePath),
 	}
-	resp, _ := svc.ListObjects(params)
-
+	resp, err := svc.ListObjects(params)
+	if err != nil {
+		fmt.Println(err)
+	}
 	// Iterate over objects in bucket with prefix.
-	fmt.Printf("Number of objects in set:, %d\n", len(resp.Contents))
+	fmt.Printf("Number of objects in set: %d\n", len(resp.Contents))
+	fmt.Println(resp)
 	for _, record := range resp.Contents {
 		fmt.Printf("Working on: %s\n", *record.Key)
 
 		//Create a file to hold original image data.
 		file, err := os.Create("/tmp/file.jpg")
 		if err != nil {
-			exitErrorf("Unable to open file %q, %v", err)
+			exitErrorf("Unable to open fi:e %q, %v", err)
 		}
 		defer file.Close()
 
@@ -68,44 +95,33 @@ func main() {
 
 		// process and upload defined file sizes.
 		// TODO: seperate processing and uploading and make concurrent.
-		// TODO: iterate defined sizes from flags.
-		maxwell.SquareResize(file,
-			1200,
-			*bucket,
-			fmt.Sprintf("%s/%s_1200w.jpg",
-				*cachePath,
-				maxwell.Basename(*record.Key)),
-			uploader)
-		maxwell.SquareResize(file,
-			400,
-			*bucket,
-			fmt.Sprintf("%s/%s_400w.jpg",
-				*cachePath,
-				maxwell.Basename(*record.Key)),
-			uploader)
+		for _, o := range outputs {
+			if o.Type == "square" {
+				maxwell.SquareResize(file,
+					uint(o.Size),
+					*bucket,
+					fmt.Sprintf("%s/%s_%dw.jpg",
+						*cachePath,
+						maxwell.Basename(*record.Key), o.Size),
+					uploader)
+			}
+			if o.Type == "svg" {
+				// Create a blurred svg.
+				svg := maxwell.ConvertToSVG("/tmp/file.jpg")
 
-		maxwell.SquareResize(file,
-			100,
-			*bucket,
-			fmt.Sprintf("%s/%s_100w.jpg",
-				*cachePath,
-				maxwell.Basename(*record.Key)),
-			uploader)
-
-		// Create a blurred svg.
-		svg := maxwell.ConvertToSVG("/tmp/file.jpg")
-
-		_, err = uploader.Upload(&s3manager.UploadInput{
-			Bucket: aws.String(*bucket),
-			Key: aws.String((fmt.Sprintf("%s/%s.svg",
-				*cachePath,
-				maxwell.Basename(*record.Key)))),
-			Body:        strings.NewReader(svg),
-			ContentType: aws.String("image/svg+xml"),
-		})
-		if err != nil {
-			// Print the error and exit.
-			exitErrorf("Unable to upload %q to %q, %v", *record.Key, *bucket, err)
+				_, err = uploader.Upload(&s3manager.UploadInput{
+					Bucket: aws.String(*bucket),
+					Key: aws.String((fmt.Sprintf("%s/%s.svg",
+						*cachePath,
+						maxwell.Basename(*record.Key)))),
+					Body:        strings.NewReader(svg),
+					ContentType: aws.String("image/svg+xml"),
+				})
+				if err != nil {
+					// Print the error and exit.
+					exitErrorf("Unable to upload %q to %q, %v", *record.Key, *bucket, err)
+				}
+			}
 		}
 
 		fmt.Printf("Successfully uploaded %q to %q\n", *record.Key, *bucket)
---
+++ go.mod
@@ -0,0 +1,11 @@
+module github.com/SteveCastle/maxwell
+
+require (
+	github.com/SteveCastle/primitive v0.0.0-20170926175833-69506c928bef
+	github.com/aws/aws-sdk-go v1.15.21
+	github.com/fogleman/gg v1.1.0 // indirect
+	github.com/golang/freetype v0.0.0-20170609003504-e2365dfdc4a0 // indirect
+	github.com/nfnt/resize v0.0.0-20180221191011-83c6a9932646
+	github.com/oliamb/cutter v0.2.2
+	golang.org/x/image v0.0.0-20180708004352-c73c2afc3b81 // indirect
+)
